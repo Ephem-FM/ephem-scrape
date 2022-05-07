e072c7280e5ded38f903dde702666bda559e79b4
Max McCready <msmccready@gmail.com>
2022-05-06 22:28:35 -0400

    dynamically grabbing the date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from datetime import date
 import os
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
@@ -7,7 +8,8 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-url = "https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/day?date=2022-05-05&format=html&hide_amazon=false&hide_itunes=false&hide_arkiv=false"
+today = str(date.today().strftime('%Y-%m-%d'))
+url = f"https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/day?date={today}&format=html&hide_amazon=false&hide_itunes=false&hide_arkiv=false"
 
 driver.get(url)
 print(driver.page_source)
