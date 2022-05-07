ae01af3f0ec89bf2c2878b68b5578543d75449fe
Max McCready <msmccready@gmail.com>
2022-05-06 22:21:40 -0400

    swapped url

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get("https://medium.com")
+
+url = "https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/day?date=2022-05-05&format=html&hide_amazon=false&hide_itunes=false&hide_arkiv=false"
+
+driver.get(url)
 print(driver.page_source)
 print("Finished!")
