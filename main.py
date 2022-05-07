a75902d9caba9fd050af2ff9176812f04dc70899
Max McCready <msmccready@gmail.com>
2022-05-06 23:18:20 -0400

    getting weekdays shows

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+    # for day's playlist
 from selenium import webdriver
 from datetime import date
 from datetime import timedelta
 import os
 import re
+
+    # for day's schedule
+import calendar
+import requests
+from bs4 import BeautifulSoup as bs4
+import lxml
 
 def main():
     chrome_options = webdriver.ChromeOptions()
@@ -42,5 +49,68 @@ def main():
         
     print("Finished!")
 
+def wkshows():
+    headers = {
+        'authority': 'api.composer.nprstations.org',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'sec-ch-ua-mobile': '?1',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Mobile Safari/537.36',
+        'sec-ch-ua-platform': '"Android"',
+        'origin': 'https://kutx.org',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://kutx.org/',
+        'accept-language': 'en-US,en;q=0.9',
+    }
+    params = (
+        ('date', str(date.today().strftime('%Y-%m-%d'))),
+        ('format', 'json'),
+    )
+    
+    day = []
+    today = calendar.day_name[date.today().weekday()].lower()
+    wkday = f"kutx-{today}"
+    response = requests.get('https://kutx.org/program-schedule/', headers=headers, params=params)
+    soup = bs4(response.text,'lxml')
+    shows = soup.find("div", id=wkday).find_all("div", class_="kutx-schedule-list-item")
+    # print(wkday)
+    # print(shows)
+    for show in shows:
+        name = show.find("div", class_="kutx-schedule-list-host")
+        if(name):
+            host = name.text
+            title = show.find("div", class_="kutx-schedule-list-title").text
+            begin = show.find("div", class_="kutx-schedule-list-time").text.split("-")[0].split(" ")
+            if(begin[0] == 12 and begin[1] == "am"):
+                start = 0
+            elif(begin[1] == "am"):
+                start = begin[0]
+            elif(begin[0] == "12" and begin[1] == "pm"):
+                start = 12
+            elif(begin[1] == "pm" and begin[0] != 12):
+                start = int(begin[0]) + 12
+            
+            fin = show.find("div", class_="kutx-schedule-list-time").text.split("-")[1].split(" ")
+            if(fin[0] == 12 and fin[1] == "am"):
+                end = 0
+            elif(fin[1] == "am"):
+                end = fin[0]
+            elif(fin[0] == "12" and fin[1] == "pm"):
+                end = 12
+            elif(fin[1] == "pm" and fin[0] != 12):
+                end = int(fin[0]) + 12
+            
+            day.append({
+                'start': start,
+                'end': end,
+                'title': title,
+                'host': host
+            })
+    
+    print(day)
+    return day
+
 if __name__=="__main__":
-    main()
+    wkshows()
