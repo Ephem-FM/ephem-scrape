dd95db75ac8871440a86e82f0801c933d0582b00
Max McCready <msmccready@gmail.com>
2022-05-06 22:40:31 -0400

    changed to yesterday's playlist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from datetime import date
+from datetime import timedelta
 import os
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
@@ -8,9 +9,10 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-today = str(date.today().strftime('%Y-%m-%d'))
-print(today)
-url = f"https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/day?date={today}&format=html&hide_amazon=false&hide_itunes=false&hide_arkiv=false"
+
+yesterday = str(date.today()- timedelta(days = 1))
+print(yesterday)
+url = f"https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/day?date={yesterday}&format=html&hide_amazon=false&hide_itunes=false&hide_arkiv=false"
 print(url)
 
 driver.get(url)
