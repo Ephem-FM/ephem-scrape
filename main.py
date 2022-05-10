3c53b51a04130ea88440c4aad13bcd53e714329a
Max McCready <msmccready@gmail.com>
2022-05-09 21:21:20 -0400

    both stations now in main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import kutx989
+import koop917
+import calendar
+from datetime import date
 
 def main():
     kutx989.main()
+    today = calendar.day_name[date.today().weekday()]
+    if today == "Wednesday":
+        koop917.main()
 
 if __name__=="__main__":
     main()
-    print("finished in main!")
