78ddaf8dd535588fc6d6c49db4168e1f8f65a35d
Max McCready <msmccready@gmail.com>
2022-05-09 20:46:38 -0400

    broke kutx989 up further

--- kutx989.py
+++ kutx989.py
@@ -11,20 +11,10 @@ import requests
 from bs4 import BeautifulSoup as bs4
 import lxml
 
-def main():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-    yesterday = str(date.today()- timedelta(days = 1))
-    url = f"https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/day?date={yesterday}&format=html&hide_amazon=false&hide_itunes=false&hide_arkiv=false"
-    day = get_days_shows()
-
-    driver.get(url)
-    total_tracks = driver.page_source.split('daily-track-data-column')[1:-1]
+def main(): 
+    days_shows = get_days_shows()
+    total_tracks = get_days_songs()
+    
     for t in total_tracks:
         song = { 'station': 'kutx989'}
         details = t.split("song-data")
@@ -43,7 +33,7 @@ def main():
             time = result[-4]
             hour = get_army_time(time)
 
-            for show in day:
+            for show in days_shows:
                 if(int(hour) >= int(show["start"]) and int(hour) < int(show["end"])):
                     song["show"] = show["title"]
                     song["date"] = date.today().strftime('%Y-%m-%d')
@@ -129,6 +119,20 @@ def get_days_shows():
             })
 
     return day
+
+def get_days_songs():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    yesterday = str(date.today()- timedelta(days = 1))
+    url = f"https://api.composer.nprstations.org/v1/widget/50ef24ebe1c8a1369593d032/day?date={yesterday}&format=html&hide_amazon=false&hide_itunes=false&hide_arkiv=false"
+    driver.get(url)
+    total_tracks = driver.page_source.split('daily-track-data-column')[1:-1]
+    return total_tracks
 
 if __name__=="__main__":
     main()
