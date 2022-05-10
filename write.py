875fa807deefe47a1ed17d311e864f53560233e0
Max McCready <msmccready@gmail.com>
2022-05-09 20:57:24 -0400

    changed postgres uri to env variable

--- write.py
+++ write.py
@@ -1,8 +1,10 @@
 import psycopg2
 import uuid
+import os
 
 def pg(song):
 	con = psycopg2.connect('postgres://hnewxezrserycc:fa7730d9660660f7dc0292e1282e327c0a93ff49d062cf25f270d30bc27747e3@ec2-3-209-61-239.compute-1.amazonaws.com:5432/d26q7d7nbt04qe')
+	con = psycopg2.connect(os.environ.get("POSTGRES_URI"))
 	cur = con.cursor()
 	print("writing ", song)
 	insert_query = """ INSERT INTO playlists (playlist_song_id, station, show, date, artist, track) VALUES (%s, %s, %s, %s, %s, %s); """
