e5366ff05ac79226aa91529ad6a153ebdb6dd8e4
Max McCready <msmccready@gmail.com>
2022-06-22 20:19:33 -0400

    now to playlists

--- write.py
+++ write.py
@@ -18,7 +18,7 @@ def connect(func, **kwargs):
 	return wrapper
 
 def write_song(song, cur):
-	insert_query = """ INSERT INTO showverviews (playlist_song_id, station, show, date, artist, track) VALUES (%s, %s, %s, %s, %s, %s); """
+	insert_query = """ INSERT INTO playlists (playlist_song_id, station, show, date, artist, track) VALUES (%s, %s, %s, %s, %s, %s); """
 	insert_tuple = (uuid.uuid4().hex), str(song["station"]), str(song["show"]), song["date"], str(song["artist"]), str(song["track"])
 	cur.execute(insert_query, insert_tuple)
 
